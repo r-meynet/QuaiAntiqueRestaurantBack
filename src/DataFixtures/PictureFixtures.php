26d50c2670e6031bc38a1790574dce81dd8a11f0
r-meynet <remy.meynet@outlook.fr>
2026-02-02 11:00:03 +0100

    Ajout Restaurant & Picture Fixtures

---
+++ src/DataFixtures/PictureFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Picture;
+use App\Entity\Restaurant;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PictureFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            $picture = (new Picture())
+                ->setTitle('Image n°' . $i)
+                ->setSlug('slug-article-title-' . $i)
+                ->setRestaurant($this->getReference('restaurant' . random_int(1, 20), Restaurant::class))
+                ->setCreatedAt(new DateTimeImmutable());
+
+            $manager->persist($picture);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [RestaurantFixtures::class];
+    }
+}
---
+++ src/DataFixtures/RestaurantFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Restaurant;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class RestaurantFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            $restaurant = (new Restaurant())
+                ->setName('Restaurant n°' . $i)
+                ->setDescription('Description restaurant n°' . $i)
+                ->setAmOpeningTime([])
+                ->setPmOpeningTime([])
+                ->setMaxGuest(random_int(10, 50))
+                ->setCreatedAt(new DateTimeImmutable());
+
+            $manager->persist($restaurant);
+            $this->addReference('restaurant'.$i, $restaurant);
+        }
+
+        $manager->flush();
+    }
+}
