065a0c9abc50f9235801ce60496cda2a8bda9d7d
r-meynet <remy.meynet@outlook.fr>
2026-02-03 00:12:42 +0100

    Retrait des fixtures du démarrage prod

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "🗄️ Exécution des migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
 
-echo "🌱 Chargement des fixtures..."
-php bin/console doctrine:fixtures:load --no-interaction
-
 echo "✅ Démarrage d'Apache..."
 apache2-foreground
